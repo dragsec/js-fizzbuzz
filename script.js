00155548e1d181cb519b8cfa7f810c50fe5aa771
dragsec <draganyes@gmail.com>
2021-04-12 15:07:31 +0200

    beta

---
+++ script.js
@@ -0,0 +1,19 @@
+listaNum = []
+
+for (var i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        listaNum.push("FizzBuzz");
+    }else if(i % 5 === 0){
+        listaNum.push("Buzz");
+    }else if(i % 3 === 0){
+        listaNum.push("Fizz");
+    }else{
+        listaNum.push(i)
+    }
+} 
+
+
+console.log(listaNum);
+document.getElementById("listanumeri").innerHTML=listanNum;
+
+
